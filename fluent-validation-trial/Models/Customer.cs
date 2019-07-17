b91efe75cc6254e80a5d58f24f4c31e3c64e4023
Mike <mike@mojomortgages.com>
2019-07-17 17:09:56 +0100

    Added field properties.

--- fluent-validation-trial/Models/Customer.cs
+++ fluent-validation-trial/Models/Customer.cs
@@ -2,6 +2,9 @@
 {
     public class Customer
     {
+        public string Title { get; set; }
         public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
     }
 }
