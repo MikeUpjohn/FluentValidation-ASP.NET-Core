923da216f24932a54a609a4bac33962091f68341
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2019-07-17 17:53:57 +0100

    Implemented basic fluent validation logic for a postman request.

---
+++ fluent-validation-trial/Validators/Class.cs
@@ -0,0 +1,21 @@
+﻿using fluent_validation_trial.Models;
+using FluentValidation;
+
+namespace fluent_validation_trial.Validators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title is a required field.");
+            RuleFor(x => x.Title).MinimumLength(2).WithMessage("Title must be at least 2 characters");
+            RuleFor(x => x.Title).Custom((x, y) =>
+              {
+                  if(x.Contains("M"))
+                  {
+                      y.AddFailure("Title contains the letter M!");
+                  }
+              });
+        }
+    }
+}
